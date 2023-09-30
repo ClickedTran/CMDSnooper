b086705b19532fbe7c3c4f0b4875054118bcc498
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-09-30 13:15:36 +0700

    Add files via upload

---
+++ src/cmdsnooper/EventListener.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace cmdsnooper;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\CommandEvent;
+use cmdsnooper\CmdSnooper;
+use pocketmine\player\Player;
+
+class EventListener implements Listener {
+	public $plugin;
+	protected Player $sender;
+	public function __construct(CmdSnooper $plugin) {
+		$this->plugin = $plugin;
+	}
+
+	public function getPlugin() {
+		return $this->plugin;
+	}
+	
+	public function getPlayer() {
+	  return $sender;
+	}
+	
+	public function onCmnd(CommandEvent $event): void{
+	  $sender = $event->getSender();
+		$msg = $event->getCommand();
+		
+		if($this->getPlugin()->setting->get("Console.Logger") == "true") {
+		  if($sender instanceof Player){
+		   if($msg[0] == "login" or $msg[0] == "log" or $msg[0] == "register" or $msg[0] == "reg"){
+		     $this->getPlugin()->getLogger()->info($sender->getName() . "§a Covered for security reasons!");
+		     return;
+		   }
+		   $this->getPlugin()->getLogger()->info("§f§l[§cSpy§f]§r§a ". $sender->getName() . " §e➼§a /" . $msg);
+		   }
+	  	}
+			
+			if(!empty($this->getPlugin()->snoopers)) {
+				foreach($this->getPlugin()->snoopers as $snooper) {
+			    if($sender instanceof Player){
+			      if($msg[0] == "login" or $msg[0] == "log" or $msg[0] == "register" or $msg[0] == "reg"){
+		           $this->getPlugin()->getLogger()->info($sender->getName() . "§a Covered for security reasons!");
+		           return;
+		         }
+						 $snooper->sendMessage("§f§l[§cSpy§f]§r§a " . $sender->getName() . " §e➼§a /" . $msg);
+				  }
+				}
+	    }		
+    }
+  }
